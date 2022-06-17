d6cd6b17dbbf2666b81b1166b36ba9bff4940e1e
NathanLiew214 <66583135+NathanLiew214@users.noreply.github.com>
2022-06-17 16:16:09 -0500

    Update leet3.js

--- leet3.js
+++ leet3.js
@@ -1,11 +1,16 @@
 /**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/**
 26. Remove Duplicates from Sorted Array
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function(nums) {
     
-
+/* fill in _ for duplicates*/
       for (let i = 0; i < nums.length-1; i++){
             for (let j= i+ 1; j< nums.length; j++){          
                 if(nums[i] == nums[j]){
@@ -14,14 +19,19 @@ var removeDuplicates = function(nums) {
             }
         }
     
-     let newArray = new Array(3);
+    
+/*create new Array and push non empty into new Array*/
+     let newArray = [];
      for (let k = 0; k < nums.length; k++){
        if(nums[k] !== '_'){
            newArray.push(nums[k]);
        }
-         
+ 
      }
-     for (let m = 0; m < nums.length; m++){
+    
+    /*empty nums array and copyover newArray to nums*/
+    nums.length = 0;
+     for (let m = 0; m < newArray.length; m++){
          nums[m] = newArray[m];
      }
 
